35d0dbb6de17f5b99d734398e7a19a3b17748eff
nash <1678240316@qq.com>
2018-08-03 21:33:18 +0800

    bug优化

---
+++ BingMaYong/Assets/Scripts/FixedWindow.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedWindow : MonoBehaviour {
+
+    void Start()
+    {
+        Screen.SetResolution(1080, 1920, false);
+    }
+}
